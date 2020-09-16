d01e38d6ae9d09915d2bb32fe0993768c7be0d6e
Glo-ma <66226577+Glo-ma@users.noreply.github.com>
2020-09-16 06:09:14 -0700

    C language

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+*
+*main- beginning of program
+*
+*return value = 0
+*/
+int main ()
+{
 puts("Programming is like building a multilingual puzzle");
+return(0);
+}

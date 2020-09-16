97db4677e5cc295649772819a9d27c6e5deea649
Glo-ma <66226577+Glo-ma@users.noreply.github.com>
2020-09-16 06:10:37 -0700

    C language

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,8 @@
 *
 *return value = 0
 */
-int main ()
+int main (void)
 {
-puts("Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return(0);
 }

e4c7add02e8b99f672c86f99650ad3a0b5dd7b60
Glo-ma <ademolaglory2003@gmail.com>
2020-09-16 13:22:27 +0000

    C language

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,12 @@
-#!/bin/bash
+#include <stdio.h>
 
+/**
+* main- Beginning of code
+*
+* return value
+*/
+int main(void)
+{
+         printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+          return(0);
+}

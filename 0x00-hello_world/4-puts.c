5d8d020f295ae40e74ba82a0740ecf949a6c84de
Glo-ma <ademolaglory2003@gmail.com>
2020-09-16 13:03:46 +0000

    Displaying the words

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,14 @@
-#!/bin/bash
+ #include <stdio.h>
+
+/**
+*
+*main- beginning of program
+*
+*return value = 0
+*/
+int main(void)
+{
+       puts("\"Programming is like building a multilingual puzzle");
+       return(0);
+}
 

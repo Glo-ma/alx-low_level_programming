73e2d0bec947fa06581792eb795d5e54001fc7bb
Glo-ma <ademolaglory2003@gmail.com>
2020-09-18 15:13:24 +0000

    Working with if...else conditions in C

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 /**
-<<<<<<< HEAD
 * main - main block
 * Return: 0
 */
@@ -13,21 +12,4 @@ putchar(i + '0');
 }
 putchar('\n');
 return (0);
-=======
- * main - main block
- * 
- * Return: 0
- */
-int main(void)
-{
-	int i;
-
-	for (i = 0; i < 10; i++)
-	{
-		putchar(i + '0');
-	}
-	putchar('\n');
-
-	return (0);
->>>>>>> 4c2b40a12fc5bff39d635f6c818e87f72a44b589
 }

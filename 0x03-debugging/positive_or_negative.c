8e7561cbb6d5cf67cbd0bcff540614e9471c5277
Glo-ma <ademolaglory2003@gmail.com>
2020-09-23 10:23:01 +0000

    Debugging

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,15 +1,17 @@
-#include "holberton.h"
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 /**
 * main - Entry point
 *
 *return (0)
 */
-void positive_or_negative(int i)
+void positive_or_negative(int n)
 {
 if (n > 0)
-printf("%i is positive\n", n);
+printf("%d is positive\n", n);
 else if (n < 0)
-printf("%i is negative\n", n);
+printf("%d is negative\n", n);
 else
-printf("%i is zero\n", n);
+printf("%d is zero\n", n);
 }

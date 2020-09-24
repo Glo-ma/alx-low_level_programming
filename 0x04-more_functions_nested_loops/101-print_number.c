9dd75798f3dc198fe1d7a23b37e0bac512e286c6
Glo-ma <ademolaglory2003@gmail.com>
2020-09-24 17:46:42 +0000

    working with functions and loops

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,42 +1,37 @@
 #include "holberton.h"
+
 /**
- * print_number - print numbers out
- * @n: takes in an integer
+ * print_number - function that prints an integer..
+ * @n: input value to check
+ * Return: nothing.
  */
+
 void print_number(int n)
 {
-	int i, j, div;
-	int x = n;
-
-	div = 10;
+	unsigned int num, num2;
+	int i;
+	int aux = 1;
 
 	if (n < 0)
 	{
-		x = x * -1;
+		n = n * -1;
 		_putchar('-');
 	}
-	else if (n == 0)
+	num = n;
+	num2 = num;
+	if (num > 9)
 	{
-		_putchar('0');
+		while (num >= 10)
+		{
+			aux = aux * 10;
+			num = num / 10;
+		}
+		_putchar((num2 / aux) + '0');
+		aux = aux / 10;
+
+		for (i = aux; i >= 1; i = i / 10)
+			_putchar((num2 / i) % 10 + '0');
 	}
 	else
-	{
-		for (i = 1; x > 0; i++)
-		{
-			x /= 10;
-		}
-		for (j = 0; j < i - 1; j++)
-		{
-			div *= 10;
-
-
-			while (div >= 1)
-			{
-				_putchar(n / div + '0');
-				n = n % div;
-				div = div / 10;
-
-			}
-		}
-	}
+		_putchar(num + '0');
 }
